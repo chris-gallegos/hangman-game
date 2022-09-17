fae922ce25df6fdacc03dfc38b559ffd71cf4a5b
chris-gallegos <chrisgallegos101@gmail.com>
2022-09-17 10:23:23 -0700

    Added basic html and JS to start

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log('Js is running')
+
+/*----- constants -----*/
+// An Array containing a list of Arrays contaning the possible winning combos. 
+const WIN_WORDS = [
+    [javascript],
+    [swift],
+    [java],
+    [python],
+    [html],
+    [css],
+    [ruby],
+    [binary]
+]
