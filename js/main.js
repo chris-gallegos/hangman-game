2a905cc3332b7d9861f8b63f702cefb8202b247e
chris-gallegos <chrisgallegos101@gmail.com>
2022-09-19 18:39:02 -0700

    Added comments and organized code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-console.log('Js is running')
-
 /*----- constants -----*/
-// An Array containing a list of Arrays contaning the possible winning combos. 
+//An array of strings contaning the possible winning combos. 
 const WIN_WORDS = [
     'javascript',
     'swift',
@@ -13,31 +11,27 @@ const WIN_WORDS = [
     'binary'
 ]
 
+//An array of strings contaning the alphabet for the keyboard. 
 const LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
 
 
 /*----- app's (variables) -----*/
-
 let word = ''
 let wordPicked = null
 let choice = []
 let mistakesHad = 7
 let getWrong = 0
 
-
 /*----- cached element references -----*/
-
 const attemptDisplayEl = document.getElementById('mistakes')
 let lettersEl = document.getElementById('letters')
 const wordsEl = document.getElementById('guessword')
 
 
 /*----- functions -----*/
-
-
-//Function that genterates buttons for letters to be selected "keyboard"
+//Genterates buttons for letters to be selected "keyboard"
 function makeKeys() {
     let buttonKeys = LETTERS.map(key =>
       `
@@ -51,24 +45,20 @@ function makeKeys() {
       `).join('');
   
     lettersEl.innerHTML = buttonKeys;
-  }
+}
   
-// Function that generates the word from the array of words available 
+//Generates the word from the array of words available 
 function getRandomWord() {
     word = WIN_WORDS[Math.floor(Math.random() * WIN_WORDS.length)]
 }
 
-
-//Function that puts word generated on board
+//Puts word generated on board
 function wordOnBoard() {
     wordPicked = word.split('').map(character => (choice.indexOf(character) >= 0 ? character : " _ ")).join('')
     wordsEl.innerHTML = wordPicked
 }
 
-
-
 //Function that guesses word picked for user to guess
-
 function getChoice(letterPicked) {
     choice.indexOf(letterPicked) === -1 ? choice.push(letterPicked) : null
     document.getElementById(letterPicked).setAttribute('disabled', true)
@@ -83,7 +73,7 @@ function getChoice(letterPicked) {
     }
 }
 
-
+//Executes if user guesses the correct word
 function userWon() {
     if (wordPicked === word) {
         lettersEl.innerHTML = 'Congratulations, you win! 🤗'  
@@ -91,23 +81,23 @@ function userWon() {
     }
 }
 
-
+//Executes if user gets the answer wrong
 function userLost() {
     if (mistakesHad === getWrong) {
-        lettersEl.innerHTML = 'You Lost 😥'
+        lettersEl.innerHTML = 'You Lost 😥 <br> The correct word was: ' + word
         reload()
     }
 }
 
-
+//Part of the counter sequence
 function updateGetWrong() {
     attemptDisplayEl.innerHTML = mistakesHad
 }
 
-//To display amount of wrong guesses made
+//Displays the number of wrong guesses the user gets to start with
 attemptDisplayEl.innerHTML = mistakesHad
 
-//Restart Game
+//Restarts game and triggers when user clicks 'play again' button after winning or losing 
 function reload() {
     let btn = document.createElement('button')
     btn.innerText = 'Click here to play again'
@@ -117,7 +107,7 @@ function reload() {
     })
 }
 
-//Game Start
+//Game Start, calling functions 
 makeKeys()
 getRandomWord()
 wordOnBoard()
